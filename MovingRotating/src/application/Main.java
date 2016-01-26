27bf9d99c45f1e4b4cd5e146b7bd550deab5bb31
Atrue <wtf.ozu@gmail.com>
2016-01-26 17:52:24 +0300

    a

--- MovingRotating/src/application/Main.java
+++ MovingRotating/src/application/Main.java
@@ -2,56 +2,53 @@ package application;
 	
 import java.util.Random;
 
-import com.sun.javafx.geom.Vec2f;
-
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.LabelBuilder;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.event.Event;
-import javafx.event.EventHandler;
+import javafx.util.Duration;
 
 public class Main extends Application {
 	private int windowWidth = 600;  
 	private int windowHeight = 600;
 	private MyCanvas canvas; 
-	    
+	private Someone a;
+	private Group root;    
+	private static Random r = new Random();
 	private Scene createScene(){
 		
-		Group root = new Group();  
+		root = new Group();  
 		Scene myScene = new Scene(root, windowWidth, windowHeight);  
 		canvas = new MyCanvas(myScene);
 		root.getChildren().add(canvas); 
-		MyLabel a = new MyLabel("Some text", canvas, 250, 400);  
+		a = new Someone("Some text", canvas, 250, 100);  
 	    root.getChildren().add(a); 
-		
-		for(int i =0; i < 5; i++) {  
-			MyLabel b = new MyLabel("Some text", canvas, 100 + 70*i, 50 + 13*i);  
-			root.getChildren().add(b);  
-		}
 		myScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
 
 			 @Override  
 	            public void handle(KeyEvent keyEvent) {  
-	                if (keyEvent.getCode() == KeyCode.ENTER)// Если зажата кнопка Ентер   
-	                   //pool.select(null);               //  Снять выделение  
-	                   canvas.repaintContext();         // Перерисовать  
+	                a.keyPress(keyEvent);
 	            }  
 	              
+		});
+		myScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override  
+            public void handle(KeyEvent keyEvent) {  
+                a.keyUp(keyEvent);
+            }
 		});
 		return myScene; 
 	    
 	} 
-	
+	public void update(){
+		a.update();
+	}
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -62,6 +59,14 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm()); 
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			
+			
+			Timeline timeline = new Timeline(new KeyFrame(
+			        Duration.millis(33),
+			        ae -> update()));
+			timeline.setCycleCount(Animation.INDEFINITE);
+			timeline.play();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
--- MovingRotating/src/application/MyLabel.java
+++ MovingRotating/src/application/MyLabel.java
@@ -6,12 +6,43 @@ import java.util.Random;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
+import javafx.geometry.Point3D;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
-public class MyLabel extends Label implements EventHandler<MouseEvent> {  
-	  
+public class MyLabel extends Label {  
+
+	
+private double angle = 0;	
+
+private double dX = 0;
+private double dY = 0;
+private double dAngle = 0;	
+private double dSpeed = 0;
+
+
+private double maxAngle = 3;
+
+
+
+private double maxSpeed = 10;
+private double lostSpeed = 0.01;
+private double brakSpeed = 0.15;
+private double lostTurnKoef = 0.01;
+private double addSpeed = 0.04;
+private double addPostSpeed = 0.015;
+
+private boolean _up = false;
+private boolean _left = false;
+private boolean _right = false;
+private boolean _down = false;
+
+
+
+
 private static Random r = new Random(); // ����������� ������  
 private boolean isSelected = false;    // ������ ������ �� ��� ������  
 private Point2D diff;                 // ���������� ������� ���� ������������ ������ �������,  
@@ -25,50 +56,26 @@ public MyLabel(String text, MyCanvas canvas){      //���������
    setPrefWidth(100);  
    setPrefHeight(50); 
    setId("my_label");
-   setEventHandler(MouseEvent.ANY, this);
     setAlignment(Pos.CENTER);  
     X = r.nextDouble() * 500;
     Y = r.nextDouble() * 300 + 200;
     setLayoutY(X);  
     setLayoutX(Y);  
-    setRotate(r.nextDouble() * 180);  
+    setRotate(angle);  
 }
 public MyLabel(String text, MyCanvas canvas, int x, int y){
 	   super(text);  
 	   this.canvas = canvas; 
-	   setPrefWidth(100);  
-	   setPrefHeight(50); 
+	   setPrefWidth(30);  
+	   setPrefHeight(70); 
 	   setId("my_label");
-	   setEventHandler(MouseEvent.ANY, this);
 	    setAlignment(Pos.CENTER);  
 	    X = x;
 	    Y = y;
 	    setLayoutX(X);  
 	    setLayoutY(Y);  
 }
-
-@Override
-public void handle(MouseEvent mouseEvent) {
-	// TODO Auto-generated method stub
-	if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED){  
-        //if (!isSelected) select(true);
-		select(!isSelected);
-    } else  
-    if (mouseEvent.getEventType() == MouseEvent.MOUSE_PRESSED){  
-    	select(!isSelected);
-        diff = new Point2D(-mouseEvent.getSceneX() + getLayoutX(), -mouseEvent.getSceneY() + getLayoutY());  
-        
-        	
-    } else  
-    if (mouseEvent.getEventType() == MouseEvent.MOUSE_RELEASED){  
-        diff = null;  
-    } else  
-    if (mouseEvent.getEventType() == MouseEvent.MOUSE_DRAGGED && diff != null){  
-        setLayoutX(mouseEvent.getSceneX() + diff.getX());  
-        setLayoutY(mouseEvent.getSceneY() + diff.getY());  
-        canvas.repaintContext();  
-    }  
-}  
+  
 public double getCenterX(){  
     return getLayoutX() + getPrefWidth()/2;  
 }  
@@ -97,5 +104,83 @@ public void select(boolean fl){
         setId("my_label");  
     } 
 }
-  
+public void update(){
+	if (_left && !_right){
+		dAngle = -maxAngle * Math.min(1, dSpeed);
+		dSpeed -= dSpeed * lostTurnKoef - lostSpeed;
+	}
+	if (_right && !_left){
+		dAngle = maxAngle * Math.min(1, dSpeed);
+		dSpeed -= dSpeed * lostTurnKoef - lostSpeed;
+	}
+	if (_up && !_down){
+		
+		if (dSpeed < maxSpeed)
+			dSpeed += addSpeed;
+		else
+			dSpeed += addPostSpeed;
+	}
+	if (_down && !_up){
+		if (dSpeed > 0)
+			dSpeed = dSpeed > brakSpeed ? dSpeed - brakSpeed: 0;
+	}
+	
+	
+	dX = Math.sin(-angle * Math.PI / 180)*dSpeed;
+	dY = Math.cos(angle * Math.PI / 180)*dSpeed;
+	
+	
+	
+	
+	
+	System.out.println();
+	
+	
+	if ( X + dX < 0){
+		dSpeed = dSpeed - Math.abs(dX);
+		dX = -dX;
+	}
+	
+	
+	X += dX;
+	Y += dY;
+	angle += dAngle;
+	
+	
+	setLayoutX(X);
+	setLayoutY(Y);
+	setRotate(angle);
+	dAngle = 0;
+	dX = 0;
+	dY = 0;
+	dSpeed = dSpeed > lostSpeed ? dSpeed - lostSpeed: 0;
+}
+public void keyPress(KeyEvent keyEvent) {
+	if (keyEvent.getCode() == KeyCode.W){
+		_up = true;
+	}
+	if (keyEvent.getCode() == KeyCode.A){
+		_left = true;
+	}
+	if (keyEvent.getCode() == KeyCode.D){
+		_right = true;
+	}
+	if (keyEvent.getCode() == KeyCode.S){
+		_down = true;
+	}
+}
+public void keyUp(KeyEvent keyEvent){
+	if (keyEvent.getCode() == KeyCode.W){
+		_up = false;
+	}
+	if (keyEvent.getCode() == KeyCode.A){
+		_left = false;
+	}
+	if (keyEvent.getCode() == KeyCode.D){
+		_right = false;
+	}
+	if (keyEvent.getCode() == KeyCode.S){
+		_down = false;
+	}
+}
 }  
